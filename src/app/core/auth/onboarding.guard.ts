7b535dd7fd0c00ae320f246bb26834f48ae1d6f8
12jihan <jareem.e.hoff@gmail.com>
2026-06-16 16:36:24 -0400

    fix: cleared an issue where the user would get sent back to the onboarding route if they refreshed the page

--- src/app/core/auth/onboarding.guard.ts
+++ src/app/core/auth/onboarding.guard.ts
@@ -1,30 +1,39 @@
 import { inject } from '@angular/core';
 import { Router, type CanActivateFn } from '@angular/router';
 import { ProfileService } from '../services/profile.service';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from './auth.service';
+import { User } from 'firebase/auth';
 
 export const onboardingGuard: CanActivateFn = async () => {
   const profileService = inject(ProfileService);
+  const authService = inject(AuthService);
   const router = inject(Router);
 
-  // If profile is not loaded yet, fetch it
-  console.log("checking to see if this passes or fails:", profileService.profile());
+  // 1. On hard refresh, the profile signal will be null.
   if (!profileService.profile()) {
     try {
-      console.log("profile was not loaded... fetching...")
-      await profileService.getProfile();
-    } catch {
-      console.log("onboarding check failed...");
+      // 2. Wait for Firebase to figure out who the user is.
+      const firebaseUser: User | null = await firstValueFrom(authService.user$)
+
+      if (!firebaseUser) {
+        return router.createUrlTree(['/login']);
+      }
+
+      // 3. ACTUALLY fetch the profile from PostgreSQL using the UID!
+      await profileService.get_user(firebaseUser.uid);
+    } catch (error) {
+      console.error("Guard failed to fetch profile from DB:", error);
       return router.createUrlTree(['/onboarding/step-1']);
     }
   }
 
-  console.log("profileService onboarding check:", profileService.isOnboardingComplete());
-
+  // 4. Now the signal is populated. Check the DB value.
   if (profileService.isOnboardingComplete()) {
     return true;
   }
 
-  // Redirect to the appropriate onboarding step
+  // Redirect to the appropriate onboarding step if genuinely in complete;
   const step = profileService.onboardingStep() || 1;
   return router.createUrlTree([`/onboarding/step-${step}`]);
 };
